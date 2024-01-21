e239488ec4ba574fb636bcc12a43bb65c253e45e
KARAN\karan <Karandhal5050@gmail.com>
2024-01-21 00:42:32 -0800

    Added "NaughtyAttributes" so I could make clickable buttons to execute code in editor. Also Added hose which is able to add and subtract segments.

---
+++ PirateSoftwareJam14/Assets/Scripts/Hose.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+using System;
+
+public class Hose : MonoBehaviour
+{
+    [Header("Setup")]
+    [SerializeField] private GameObject hose_Start;
+    [SerializeField] private GameObject hose_End;
+    [SerializeField] private GameObject hose_Segment;
+
+    [SerializeField] private int segments = 0;
+
+    private List<GameObject> hose_Segments_Objects = new List<GameObject>();
+
+    [SerializeField] private GameObject backpack_Snap;
+
+    public Transform water_Snap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(segments / 2 + 1);
+        Debug.Log(Vector3.Distance(hose_Start.transform.position, hose_End.transform.position));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Vector3.Distance(hose_Start.transform.position, hose_End.transform.position) > segments / 2 + 1)
+        {
+            Debug.Log("Too Far");
+        }
+    }
+
+    [Button("Reset Hose")]
+    private void ResetHose()
+    {
+        hose_Start.transform.position = Vector3.zero;
+
+        for (int i = hose_Segments_Objects.Count; i > 0; i--)
+        {
+            DestroyImmediate(hose_Segments_Objects[i - 1].gameObject);
+        }
+        hose_Segments_Objects.Clear();
+
+        if (segments <= 0)
+        {
+            hose_Start.GetComponent<LookAtTarget>().target = hose_End.transform;
+
+            hose_End.transform.position = new Vector3(0.5f, 0, 0);
+        }
+        else
+        {
+
+            for (int i = 0; i < segments; i++)
+            {
+                GameObject Hose_seg = Instantiate(hose_Segment);
+                Hose_seg.transform.parent = gameObject.transform;
+                hose_Segments_Objects.Add(Hose_seg);
+                hose_Segments_Objects[i].transform.position = new Vector3(0.5f + (0.5f * i), 0, 0);
+                hose_Segments_Objects[i].GetComponent<LookAtTarget>().target = hose_End.transform;
+
+                if (i == 0)
+                {
+                    hose_Start.GetComponent<LookAtTarget>().target = hose_Segments_Objects[i].transform;
+
+                    hose_Segments_Objects[i].GetComponent<SpringJoint>().connectedBody = hose_Start.GetComponent<Rigidbody>();
+                }
+                else
+                {
+                    hose_Segments_Objects[i - 1].GetComponent<LookAtTarget>().target = hose_Segments_Objects[i].transform;
+
+                    hose_Segments_Objects[i].GetComponent<SpringJoint>().connectedBody = hose_Segments_Objects[i - 1].GetComponent<Rigidbody>();
+                }
+            }
+
+            hose_Segments_Objects[hose_Segments_Objects.Count - 1].GetComponent<LookAtTarget>().target = hose_End.transform;
+
+            hose_End.GetComponent<SpringJoint>().connectedBody = hose_Segments_Objects[hose_Segments_Objects.Count - 1].GetComponent<Rigidbody>();
+
+            hose_End.transform.position = new Vector3(0.5f * (hose_Segments_Objects.Count + 1), 0, 0);
+        }
+    }
+
+    [Button("Add Segment")]
+    private void AddSegment()
+    {
+        segments++;
+
+        ResetHose();
+    }
+
+    [Button("Subtract Segment")]
+    private void SubtractSegment()
+    {
+        segments--;
+
+        ResetHose();
+    }
+}
---
+++ PirateSoftwareJam14/Assets/Scripts/LookAtTarget.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour
+{
+    public Transform target = null;
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.LookAt(target);
+        }
+    }
+}
