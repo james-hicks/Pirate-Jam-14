dccf91e117da330001f6220bbf48c0f4d3aff6ff
Travis F <97186257+Travis-Fich@users.noreply.github.com>
2024-01-26 12:19:48 -0800

    Walking Sound Implemented on animation.

---
+++ PirateSoftwareJam14/Assets/Scripts/WalkingSound.cs
@@ -0,0 +1,52 @@
+using Cinemachine;
+using NaughtyAttributes.Test;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WalkingSound : MonoBehaviour
+{
+    public AudioClip[] FootstepSFX;
+    public AudioClip[] Footstep2SFX;
+
+    private AudioSource source;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    void FootstepEvent()
+    {
+        AudioClip clip = FootstepSFX[(int)Random.RandomRange(0,FootstepSFX.Length)];
+        source.clip = clip;
+        source.pitch = 1f;
+        source.Play();
+
+    }
+    void FootstepEvent2()
+    {
+        AudioClip clip = Footstep2SFX[(int)Random.RandomRange(0, FootstepSFX.Length)];
+        source.clip = clip;
+        source.pitch = 1f;
+        source.Play();
+
+    }
+    void SprintpEvent()
+    {
+        AudioClip clip = FootstepSFX[(int)Random.RandomRange(0, FootstepSFX.Length)];
+        source.clip = clip;
+        source.pitch = 1.25f;
+        source.Play();
+
+    }
+    void SprintpEvent2()
+    {
+        AudioClip clip = Footstep2SFX[(int)Random.RandomRange(0, FootstepSFX.Length)];
+        source.clip = clip;
+        source.pitch = 1.25f;
+        source.Play();
+
+    }
+}
