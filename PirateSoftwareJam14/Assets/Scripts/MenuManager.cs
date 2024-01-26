6062aecddee654d674f8c29e39d49b4ca4c9f58a
James-Hicks <james8hicks@gmail.com>
2024-01-26 18:33:47 -0500

    feat: main menu implemented

---
+++ PirateSoftwareJam14/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject SettingsMenu;
+    [SerializeField] private GameObject Menu;
+    [SerializeField] private GameObject HTPMenu;
+    public void OnPlay()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnSettings()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnHTP()
+    {
+        Menu.SetActive(false);
+        HTPMenu.SetActive(true);
+    }
+
+    public void OnQuit()
+    {
+        Debug.LogWarning("Quitting Game...");
+        Application.Quit();
+    }
+
+    public void BackToBaseMenu()
+    {
+        Menu.SetActive(true);
+        HTPMenu.SetActive(false);
+    }
+}
